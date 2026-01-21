76c05d89ddb91ee24d627fa7eab0931eb7dcbc5a
Euphemia1 <euphemiachikungulu347@gmail.com>
2026-01-21 15:55:01 +0200

    add chnages to ocdebase

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,12 @@
 import { updateSession } from "@/lib/auth-middleware"
 import type { NextRequest } from "next/server"
 
-export async function proxy(request: NextRequest) {
+export async function middleware(request: NextRequest) {
     return await updateSession(request)
 }
 
 export const config = {
-    runtime: 'nodejs',
+    // runtime: 'nodejs',
     matcher: [
         "/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
     ],
